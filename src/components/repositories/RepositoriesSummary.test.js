e807a1a23ac605d4088027a197314eeaff5ba05f
yichien <kevinyichienlee@gmail.com>
2024-07-08 13:50:13 +0800

    multi properties of an object use for loop and Regex

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,17 +1,20 @@
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("display the primary language of the repository", () => {
+test("displays information about the repository", () => {
   const repository = {
     language: "JavaScript",
-    star_count: 100,
-    fork: 50,
+    stargazers_count: 100,
+    forks: 50,
     open_issues: 20,
   };
 
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("JavaScript");
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
 
-  expect(language).toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  }
 });
